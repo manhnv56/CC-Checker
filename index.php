935a4cd976247e7fcac9a1739e0788bed5e2c8a7
tarekhasan404 <67327912+tarekhasan404@users.noreply.github.com>
2021-09-16 00:51:55 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = ""; // 1951678391:AAElODAa0OMjN6pVcNweJpWkrNT7ZiRYd7I
+$botToken = "1951678391:AAElODAa0OMjN6pVcNweJpWkrNT7ZiRYd7I";
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
